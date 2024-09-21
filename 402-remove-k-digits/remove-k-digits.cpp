ad0089dc53405923612389d7265d33d4d36925e0
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2024-09-21 23:26:32 +0530

    Time: 11 ms (62.96%) | Memory: 10.7 MB (21.11%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,28 +1,57 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n = num.length();
-        string ans = "";
-
-        for(int i=0; i<n; i++){
-
-            while(ans.length() > 0 && k > 0 && ans.back() > num[i]){
-                ans.pop_back();
-                k--;
-            }
-            if(num[i] != '0' || ans.length() > 0){
-                ans.push_back(num[i]);
-            }
-        }
-
-        while(ans.length() > 0 && k > 0){
-            ans.pop_back();
-            k--;
-        }
-
-        if(ans ==""){
+        if(num.length() == k) {
             return "0";
         }
-        return ans;
+        stack<char>st;
+        string ans = "";
+        st.push(num[0]);
+        
+        for(int i = 1 ; i < num.length() ; i++) {
+
+            while((!st.empty() && st.top() > num[i]) && k>0) {
+                st.pop();
+                k--;
+            }
+
+            st.push(num[i]);
+        }
+
+        // stack ko khali kardo ab
+
+        while(!st.empty()) {
+            char front = st.top();
+            ans.push_back(front);
+            st.pop();
+        }
+
+        if(k>0) {
+            ans.erase(0,k);
+        }
+
+        reverse(ans.begin(),ans.end());
+        int p = 0;
+
+        for(int i = 0 ; i < ans.length() ; i++) {
+            if(ans[i] == '0') {
+                p++;
+            }
+            else {
+                break;
+            }
+        }
+
+    
+        string res = "";
+
+        for(int i = p ; i < ans.length() ; i++) {
+            res.push_back(ans[i]);
+        }
+
+        if(res == "") {
+            return "0";
+        }
+        return res;
     }
 };

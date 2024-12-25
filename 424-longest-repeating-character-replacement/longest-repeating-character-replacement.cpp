019707266102fdc43a394fd5cb802bbaa5e77856
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2024-12-25 22:53:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (25.48%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -12,12 +12,9 @@ public:
             hash[s[r]-'A']++;
             maxf = max(maxf, hash[s[r]-'A']);
 
-            while((r-l+1)-maxf > k){
+            if((r-l+1)-maxf > k){
                 hash[s[l]-'A']--;
                 maxf = 0;
-                for(int i=0; i<26; i++){
-                    maxf = max(maxf, hash[i]);
-                }
                 l = l+1;
             }
 

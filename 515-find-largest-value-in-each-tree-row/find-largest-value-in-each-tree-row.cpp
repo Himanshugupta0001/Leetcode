3553f28bc493c2460605323eb4e22b8e851fc5c6
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-01-08 15:15:43 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (33.47%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -11,27 +11,23 @@
  */
 class Solution {
 public:
-    vector<int> largestValues(TreeNode* root) {
-        vector<int> ans;
-        if(root == NULL){
-            return ans;
-        }
-        
-        queue<TreeNode*> q;
-        q.push(root);
+    vector<int> ans;
+    void dfs(TreeNode* root, int depth){
+        if(root == NULL) return;
 
-        while(!q.empty()){
-            int n = q.size();
-            int maxi = INT_MIN;
-            while(n--){
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-                maxi = max(maxi, node->val);
-            }
-            ans.push_back(maxi);
+        if(ans.size() == depth){
+            ans.push_back(root->val);
         }
+        else{
+            ans[depth] = max(ans[depth], root->val);
+        }
+
+        dfs(root->left, depth+1);
+        dfs(root->right, depth+1);
+    }
+    vector<int> largestValues(TreeNode* root) {
+        dfs(root, 0);
+
         return ans;
     }
 };

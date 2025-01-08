7b33b639fc2a28018d4421d290de4d95ee3ca0b5
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-01-08 18:24:32 +0530

    Time: 3 ms (97.72%) | Memory: 128.1 MB (93.97%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,63 +11,34 @@
  */
 class Solution {
 public:
-    TreeNode* markParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track, int &target){
-        queue<TreeNode*> q;
-        TreeNode* targetNode = NULL;
-        q.push(root);
-        
-        while(!q.empty()){
-            TreeNode* current = q.front();
-            q.pop();
-            
-            if(current->val == target){
-                targetNode = current;
-            }
-            
-            if(current->left){
-                q.push(current->left);
-                parent_track[current->left] = current;
-            }
-            if(current->right){
-                q.push(current->right);
-                parent_track[current->right] = current;
-            }
+    int result;
+    int f(TreeNode* root, int start){
+        if(root == NULL){
+            return 0;
         }
-        return targetNode;
+
+        int lh = f(root->left, start);
+        int rh = f(root->right, start);
+
+        if(root->val == start){
+            result = max(lh, rh);
+
+            return -1;
+        }
+        else if(lh >= 0 && rh >= 0){
+            return max(lh, rh) + 1;
+        }
+        else{
+            int d = abs(lh) + abs(rh);
+            result = max(result, d);
+
+            return min(lh, rh) - 1;
+        }
+        return 0;
     }
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<TreeNode*, TreeNode*> parent_track;
-        TreeNode* targetNode = markParent(root, parent_track, start);
-        
-        unordered_map<TreeNode*, bool> visited;
-        queue<pair<TreeNode*, int>> q;
-        q.push({targetNode, 0});
-        visited[targetNode] = true;
-        
-        int t = 0;
-        
-        while(!q.empty()){
-            TreeNode* cur = q.front().first;
-            int ti = q.front().second;
-            q.pop();
-            
-            t = max(t, ti);
-            
-            if(cur->left && !visited[cur->left]){
-                q.push({cur->left, ti+1});
-                visited[cur->left] = true;
-            }
-            
-            if(cur->right && !visited[cur->right]){
-                q.push({cur->right, ti+1});
-                visited[cur->right] = true;
-            }
-            
-            if(parent_track[cur] && !visited[parent_track[cur]]){
-                q.push({parent_track[cur], ti+1});
-                visited[parent_track[cur]] = true;
-            }
-        }
-        return t;
+        f(root, start);
+
+        return result;
     }
 };

91c6a144976bfe68285fd7e2ec63cd881fe5a068
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-22 19:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (52.91%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,9 +3,12 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int i = 0;
-        for(int j=0; j<n; j++){
-            if(nums[j] != 0){
+        for(int j=1; j<n; j++){
+            if(nums[i] == 0 && nums[j] != 0){
                 swap(nums[i], nums[j]);
+                i++;
+            }
+            if(nums[i] != 0){
                 i++;
             }
         }

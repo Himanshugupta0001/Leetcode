52d8d5231a7f9dceccedeb28a45aa16ea9258882
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-31 10:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (26.47%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,65 +1,37 @@
-// class Solution {
-// public:
-//     bool check(int start, vector<int> &color, vector<vector<int>> &graph, int ini){
-//         queue<int> q;
-//         q.push(start);
-//         color[start] = ini;
-
-//         while(!q.empty()){
-//             int node = q.front();
-//             q.pop();
-
-//             for(auto &it : graph[node]){
-//                 if(color[it] == color[node]){
-//                     return false;
-//                 }
-//                 else if(color[it] == -1){
-//                     color[it] = 1-color[node];
-//                     q.push(it);
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-//     bool isBipartite(vector<vector<int>>& graph) {
-//         int V = graph.size();
-//         vector<int> color(V, -1);
-//         for(int i=0; i<V; i++){
-//             if(!color[i]){
-//                 if(check(i, color, graph, 0) == false){
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-// };
-
 class Solution {
 public:
+    bool check(int start, vector<int> &color, vector<vector<int>> &graph){
+        queue<int> q;
+        q.push(start);
+        color[start] = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(auto &it : graph[node]){
+                if(color[it] == color[node]){
+                    return false;
+                }
+                else if(color[it] == -1){
+                    color[it] = 1-color[node];
+                    q.push(it);
+                }
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
         vector<int> color(V, -1);
-        
-        for (int i = 0; i < V; ++i) {
-            if (color[i] == -1) {
-                queue<int> q;
-                q.push(i);
-                color[i] = 0;
-
-                while (!q.empty()) {
-                    int node = q.front(); q.pop();
-                    for (int neighbor : graph[node]) {
-                        if (color[neighbor] == -1) {
-                            color[neighbor] = 1 - color[node];
-                            q.push(neighbor);
-                        } else if (color[neighbor] == color[node]) {
-                            return false;
-                        }
-                    }
+        for(int i=0; i<V; i++){
+            if(color[i] == -1){
+                if(check(i, color, graph) == false){
+                    return false;
                 }
             }
         }
         return true;
     }
 };
+

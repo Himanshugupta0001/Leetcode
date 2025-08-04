d06edc6ca2084dacc145323a148f69a370622259
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-08-04 23:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (60.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     bool isValid(string s) {
         int n = s.length();
-
         stack<char> st;
-
         for(int i=0; i<n; i++){
-
             char ch = s[i];
-
-            if(ch == '(' || ch == '[' || ch == '{'){
+            if(ch=='(' || ch=='{' || ch=='['){
                 st.push(ch);
             }
             else{
                 if(!st.empty()){
                     char top = st.top();
-
-                    if((ch == ')' && top == '(') || (ch == ']' && top == '[') || (ch == '}' && top == '{')){
+                    if((ch==')' && top=='(') || (ch==']' && top=='[') || (ch=='}' && top=='{')){
                         st.pop();
                     }
                     else{
@@ -28,11 +23,7 @@ public:
                 }
             }
         }
-        if(st.empty()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        if(st.empty()) return true;
+        else return false;
     }
 };

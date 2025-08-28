7f46082f7ddc07c99b4303717f8de14e2d7e5fda
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-08-28 20:04:43 +0530

    Time: 861 ms (32.81%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -3,6 +3,5 @@ select p.product_id, IFNULL(round(sum(p.price*u.units)/sum(u.units), 2), 0.00) a
 from Prices p
 left join UnitsSold u
 on p.product_id = u.product_id
-and u.purchase_date >= p.start_date 
-and u.purchase_date <= p.end_date
-group by p.product_id;
+and u.purchase_date >= p.start_date and p.end_date >= u.purchase_date
+group by p.product_id
